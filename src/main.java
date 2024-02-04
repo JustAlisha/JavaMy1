61ed667df6d8764c4c06dfe85e4f5c1474df238b
Test <bloodreighn965@gmail.com>
2024-02-04 17:02:18 +0300

    2

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int price = 13_676;
+
+        int bonus = price\20;
+        System.out.println("Бонус: " + bonus);
+    }
+
+}
